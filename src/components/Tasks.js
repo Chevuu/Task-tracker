b91d3156f8c24a08e4acb9148171c6f959c20518
vjurisic <v.jurisic@student.tudelft.nl>
2022-04-17 13:47:28 +0200

    Added Task Component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,13 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'Feb 6th at 8:30pm',
-        reminder: false,
-    },
-    {
-        id: 3,
-        text: 'Gym',
-        day: 'Feb 8th at 12:30pm',
-        reminder: true,
-    }
-]
+import Task from "./Task";
 
-const Tasks = () => {
+const Tasks = ({tasks, onDelete, onToggle}) => {
   return (
     <>
-       {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))} 
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default Tasks
+export default Tasks;
